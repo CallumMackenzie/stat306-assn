a4ccd5043e3d95d845fa18340f1f422469e937e5
Matthew deHaas <mattdehaas28@gmail.com>
2026-03-23 11:40:23 -0700

    Ensure .tex document can be opened.

--- analysis.R
+++ analysis.R
@@ -5,10 +5,9 @@ library(stargazer)
 df <- read.csv("data/medical_insurance.csv")
 df <- df[df$sex %in% c("Female", "Male"), ]
 
-# Helper for saving files to disk
-save_plot <- function(path, plot) {
-  # if (!file.exists(path)) 
-	ggsave(path, plot, width = 7, height = 4.5, dpi = 150)
+# Helper for saving files to disk with a consistent size
+save_plot <- function(filename, plot) {
+	ggsave(paste0("latex/images/", filename), plot, width=7, height=4.5, dpi=150)
 }
 
 # 2.1 - Explore variables in isolation
@@ -16,59 +15,59 @@ charges_plot <-
 	ggplot(df, aes(x=annual_premium)) + 
 	geom_histogram(aes(y=after_stat(density))) + 
 	labs(title="Distribution of Insurance Charges", x="Charges (USD)", y="Density")
-save_plot("images/insurance_charges_distribution.png", charges_plot)
+save_plot("insurance_charges_distribution.png", charges_plot)
 
 log_charges_plot <- 
 	ggplot(df, aes(x=log(annual_premium))) + 
 	geom_histogram(aes(y=after_stat(density))) + 
 	labs(title="Distribution of Insurance Charges", x="Log Charges (USD)", y="Density")
-save_plot("images/log_insurance_charges_distribution.png", log_charges_plot)
+save_plot("log_insurance_charges_distribution.png", log_charges_plot)
 
 age_plot <- 
 	ggplot(df, aes(x=age)) + 
 	geom_histogram(aes(y=after_stat(density))) + 
 	labs(title="Distribution of age of Subjects", x="Age (years)", y="Density")
-save_plot("images/age_distribution.png", age_plot)
+save_plot("age_distribution.png", age_plot)
 
 bmi_plot <- 
 	ggplot(df, aes(x=age)) + 
 	geom_histogram(aes(y=after_stat(density))) + 
 	labs(title="Distribution of BMI of Subjects", x="Body Mass Index (BMI)", y="Density")
-save_plot("images/bmi_distribution.png", bmi_plot)
+save_plot("bmi_distribution.png", bmi_plot)
 
 # 2.2 - Explore Bivariate (sex)
 log_sex_plot <- 
 	ggplot(df, aes(x=age, y=log(annual_premium), fill=sex)) + 
 	geom_boxplot(alpha = 0.7, outlier.size = 0.8) + 
 	labs(title="Log Annual Insurance Premium by sex", x="Sex", y="Log Annual Insurance Premium (USD)")
-save_plot("images/log_insurance_charges_vs_sex_distribution.png", log_sex_plot)
+save_plot("log_insurance_charges_vs_sex_distribution.png", log_sex_plot)
 
 # 2.3 - Explore variables among the sexes
 age_vs_sex_plot <- 
   ggplot(df, aes(x=age, y=log(annual_premium), color=sex)) +
   geom_boxplot(alpha = 0.7, outlier.size = 0.8) +
 	labs(title="Age Vs. Sex", x="sex", y="Age (Years)")
-save_plot("images/age_vs_sex_distribution.png", age_vs_sex_plot)
+save_plot("age_vs_sex_distribution.png", age_vs_sex_plot)
 
 age_vs_log_charges_plot <-
   ggplot(df, aes(x=bmi, y=log(annual_premium), color=sex)) +
   geom_point(alpha = 0.2, size = 0.6) +
   geom_smooth(method = "lm", se = TRUE) +
 	labs(title="Age Vs. Log Charges", x="Age (Years)", y="Log Charges (USD)")
-save_plot("images/age_vs_log_charges_distribution.png", age_vs_log_charges_plot)
+save_plot("age_vs_log_charges_distribution.png", age_vs_log_charges_plot)
 
 bmi_vs_sex_plot <- 
 	ggplot(df, aes(x=sex, y=bmi, color=sex)) + 
   geom_boxplot(alpha = 0.7, outlier.size = 0.8) +
 	labs(title="Age Vs. Sex", x="sex", y="BMI (kg/m^2)")
-save_plot("images/bmi_vs_sex_distribution.png", bmi_vs_sex_plot)
+save_plot("bmi_vs_sex_distribution.png", bmi_vs_sex_plot)
 
 bmi_vs_log_charges_plot <-
 	ggplot(df, aes(x=bmi, y=log(annual_premium), color=sex)) + 
   geom_point(alpha = 0.2, size = 0.6) +
   geom_smooth(method = "lm", se = TRUE) +
 	labs(title="BMI Vs. Log Charges", x="BMI (kg/m^2)", y="Log Charges (USD)")
-save_plot("images/bmi_vs_log_charges_distribution.png", bmi_vs_log_charges_plot)
+save_plot("bmi_vs_log_charges_distribution.png", bmi_vs_log_charges_plot)
 
 # 2.4 - Linear regression 
 # Would have chosen more descriptive names but Stargazer has issues with long names for some reason
@@ -76,7 +75,7 @@ m1 <- lm(log(annual_premium) ~ sex, data=df)
 m2 <- lm(log(annual_premium) ~ sex + age + bmi, data=df)
 
 
-if (file.exists("tables/regression_table.tex")) file.remove("tables/regression_table.tex")
+if (file.exists("latex/tables/regression_table.tex")) file.remove("tables/regression_table.tex")
 table_lines <- capture.output(
   stargazer(m1, m2,
     type="latex",
@@ -90,7 +89,7 @@ table_lines <- capture.output(
   )
 )
 
-writeLines(table_lines, "tables/regression_table.tex")
+writeLines(table_lines, "latex/tables/regression_table.tex")
 
 
 print("Success!")
--- scripts/run.sh
+++ scripts/run.sh
@@ -11,19 +11,20 @@ while getopts "c" flag; do
 done
 
 # Replace old build artifacts
-rm -rf images tables .build pdf
-mkdir images tables .build pdf
+rm -rf latex/images latex/tables .build pdf
+mkdir latex/images latex/tables .build pdf
 
 if [ "$COMPILE" = true ]; then
-  docker run --platform linux/amd64 --rm -v "$(pwd):/project" stat-project bash -c "
-    Rscript analysis.R &&
-    for tex_file in latex/*.tex; do
-      pdflatex -output-directory=.build \$tex_file
-      pdflatex -output-directory=.build \$tex_file
-      filename=\$(basename \$tex_file .tex)
-      cp .build/\${filename}.pdf pdf/
-    done
-  "
+	docker run --platform linux/amd64 --rm -v "$(pwd):/project" stat-project bash -c "
+		Rscript analysis.R &&
+		cd /project/latex &&
+		for tex_file in *.tex; do
+			pdflatex -output-directory=../.build \$tex_file
+			pdflatex -output-directory=../.build \$tex_file
+			filename=\$(basename \$tex_file .tex)
+			cp ../.build/\${filename}.pdf ../pdf/
+		done
+	"
 else
   docker run --platform linux/amd64 --rm -v "$(pwd):/project" stat-project bash -c "
     Rscript analysis.R
